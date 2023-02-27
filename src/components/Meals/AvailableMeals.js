e06d33e0e48d52a8d133aa59e8c0ae101b0c5544
skadngus0323 <112064849+skadngus0323@users.noreply.github.com>
2023-02-27 13:03:50 +0900

    한글화 & ReactSteak로 변경

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,35 +5,51 @@ import classes from "./AvailableMeals.module.css";
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
+    name: "토마호크 스테이크 & 셰프 초이스 가니시",
+    description:
+      "시즌 스페셜로 제공되는 요크셔 푸딩과 매시드 포테이토 상큼한 크랜베리 소스, 스위티 드롭, 말돈 소금 등 다양한 셰프 초이스 가니시와 함께 즐기는 토마호크 스테이크",
+    price: 43.99,
   },
   {
     id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
+    name: "포터하우스 스테이크",
+    description:
+      "부드러운 안심과 풍미가 진한 등심을 함께 즐길 수 있는 ReactSteak의 프리미엄 스테이크",
+    price: 36.99,
   },
   {
     id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
+    name: "갈릭 립아이",
+    description: "구운 마늘과 마늘칩이 어우러진 꽃등심 스테이크",
+    price: 31.99,
   },
   {
     id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
+    name: "시푸드 빠삐요뜨",
+    description:
+      "각종 해산물과 채소를 프리미엄 쿠킹 필름으로 조리해 재료 본연의 맛과 향을 즐길 수 있는 토마토 수프",
+    price: 16.5,
+  },
+  {
+    id: "m5",
+    name: "투움바 스테이크 파스타",
+    description:
+      "부드러운 스테이크와 ReactSteak 최고의 투움바 소스로 맛을 낸 파스타",
+    price: 23.99,
+  },
+  {
+    id: "m6",
+    name: "치킨 텐더 샐러드",
+    description: "부드럽고 고소한 닭 안심 튀김을 올린 샐러드 ",
+    price: 12.99,
   },
 ];
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
--- src/components/Meals/MealsSummarys.js
+++ src/components/Meals/MealsSummarys.js
@@ -3,14 +3,14 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>최고의 스테이크, 당신에게로</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        ReactSteak는 딜리버리 서비스 론칭 1년만에 딜리버리 전용 매장을 포함한
+        60여개의 매장에서 운영되고 있습니다.
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        ReactSteak 셰프가 직접 만든 요리를 이제 집에서도 편안하게 즐길 수
+        있습니다.
       </p>
     </section>
   );
